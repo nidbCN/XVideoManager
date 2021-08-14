e78282177bc15b09097a6fcf4aa4a0ce54ce8b17
Gaein_nidb <mail@gaein.cn>
2021-08-14 23:43:12 +0800

    Add EntityFramework

--- XVideoManager.Core/Contexts/FileDbContext.cs
+++ XVideoManager.Core/Contexts/FileDbContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using FileContextCore;
+using XVideoManager.Core.Entities;
 
 namespace XVideoManager.Core.Contexts
 {
-    class FileDbContext
+    class FileDbContext : DbContext
     {
+        public DbSet<VideoEntity>? Videos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseFileContextDatabase();
+        }
     }
 }
