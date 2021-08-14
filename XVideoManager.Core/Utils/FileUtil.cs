c1a4f49b895954699757fa166326f6f1517fe700
Gaein_nidb <mail@gaein.cn>
2021-08-14 10:47:46 +0800

    Add utils

---
+++ XVideoManager.Core/Utils/FileUtil.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace XVideoManager.Core.Utils
+{
+    public static class FileUtil
+    {
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>文件大小（如果文件不存在则为null）</returns>
+        public static uint? GetSize(string fileName)
+        {
+            var file = new FileInfo(fileName);
+            
+            if(file.Exists)
+                return (uint)file.Length;
+
+            return null;
+        }
+    }
+}
