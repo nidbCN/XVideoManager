9a3a5a4ff0c883d709661c2ce11b7bce26134914
Gaein nidb <mail@gaein.cn>
2021-09-28 22:08:26 +0800

    Add functions

--- XVideoManager.Core/Services/IVideoService.cs
+++ XVideoManager.Core/Services/IVideoService.cs
@@ -1,38 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using XVideoManager.Core.Entities;
 
 namespace XVideoManager.Core.Services
 {
     public interface IVideoService
-    { 
+    {
+        #region 获取视频
+
         /// <summary>
         /// 获取视频列表
         /// </summary>
         /// <returns></returns>
         public IList<VideoEntity> GetVideoList();
-
-        /// <summary>
-        /// 添加视频
-        /// </summary>
-        /// <param name="videoEntity">视频实体</param>
-        public void AddVideo(VideoEntity videoEntity);
-
-        /// <summary>
-        /// 设置视频为已存储
-        /// </summary>
-        /// <param name="video">视频实体</param>
-        /// <param name="path">存储路径</param>
-        /// <param name="fragments">视频片段</param>
-        /// <param name="videoName">视频名称</param>
-        /// <param name="coverName">封面名称</param>
-        public void StorageVideo(
-            VideoEntity video,
-            string path,
-            IList<string>? fragments,
-            string? videoName = null,
-            string? coverName = null);
 
         /// <summary>
         /// 通过番号获取视频
@@ -54,12 +34,65 @@ namespace XVideoManager.Core.Services
         /// <param name="brandName">厂牌名</param>
         /// <returns>视频实体列表</returns>
         public IList<VideoEntity> GetVideoByBrand(string brandName);
-        
+
         /// <summary>
         /// 通过标签获取视频
         /// </summary>
         /// <param name="tags">标签列表</param>
         /// <returns>视频实体列表</returns>
         public IList<VideoEntity> GetVideoByTags(params string[] tags);
+
+        #endregion
+
+        #region 添加视频
+        /// <summary>
+        /// 添加视频
+        /// </summary>
+        /// <param name="videoEntity">视频实体</param>
+        public void AddVideo(VideoEntity videoEntity);
+
+        #endregion
+
+        #region 修改视频
+
+        /// <summary>
+        /// 设置视频为已存储
+        /// </summary>
+        /// <param name="video">视频实体</param>
+        /// <param name="path">存储路径</param>
+        /// <param name="fragments">视频片段</param>
+        /// <param name="videoName">视频名称</param>
+        /// <param name="coverName">封面名称</param>
+        public void StorageVideo(
+            VideoEntity video,
+            string path,
+            IList<string>? fragments,
+            string? videoName = null,
+            string? coverName = null);
+
+        public void UpdateVideo(VideoEntity video, VideoEntity newVideo);
+
+        #endregion
+
+        #region 删除视频
+
+        /// <summary>
+        /// 删除指定视频
+        /// </summary>
+        /// <param name="video">视频实体</param>
+        public void DeleteVideo(VideoEntity video);
+
+        /// <summary>
+        /// 通过番号删除视频
+        /// </summary>
+        /// <param name="code"></param>
+        public void DeleteVideoByCode(string code);
+
+        /// <summary>
+        /// 通过指定条件删除视频
+        /// </summary>
+        /// <param name="match"></param>
+        public void DeleteVideos(Func<VideoEntity, bool> match);
+        #endregion
     }
 }
