d4adcf9fb53fd6ab6a9a5088d6bb7b871f28ee1c
Gaein nidb <mail@gaein.cn>
2021-09-29 20:18:17 +0800

    Add extension method

---
+++ XVideoManager.Common/Extensions/DbContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace XVideoManager.Common.Extensions
+{
+    public static class DbContextExtension
+    {
+        public static void RemoveAll<T>(this DbSet<T> set, Predicate<T> predicate) where T : class
+        {
+            var collection = set.Where(x => predicate(x)).ToList();
+
+            collection.Clear();
+        }
+    }
+}
